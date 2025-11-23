5690be76ea868b2fc22c9e1c05fdcecee7c700b7
safia-khoulaid <safiakhoulaid11@gmail.com>
2025-11-23 22:06:27 +0100

    modifier la page home

--- pages/api/recommendations.js
+++ pages/api/recommendations.js
@@ -8,6 +8,7 @@ const recommendationCard = [
     view: "Hey everyone! I wanted to take a moment to recommend Osama Javaid for any React.js opportunities. I had the pleasure of working with him on multiple projects where he showcased a strong understanding of React.js and Next.js concepts and delivered high-quality code. He consistently demonstrated a willingness to learn and grow, and his enthusiasm for tackling new challenges was contagious. Osama is a reliable and dedicated team member who would be a great asset to any React.js team. Highly recommended! 👍! 🌟",
     linkednURL: "https://www.linkedin.com/in/shahmir-minhas/"
   },
+  
   {
     id: 1,
     name: 'Sikandar Hayat',
